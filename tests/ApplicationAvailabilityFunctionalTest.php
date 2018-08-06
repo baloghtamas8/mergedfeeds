73583d4cb500ebb97d53ef04572373e89cd90c6b
Tamás Balogh <tamas.balogh@naturalswiss.ch>
2018-08-06 18:42:16 +0200

    application availability test

---
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        yield ['/knplabs/symfony/fib'];
+        yield ['/knplabs/symfony'];
+        yield ['/knplabs/symfony/mod'];
+        // ...
+    }
+}
